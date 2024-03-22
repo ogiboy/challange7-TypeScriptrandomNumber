3025fd7184d16613dbedd323d28c3c917a73e35b
Oğuzcan Toptaş <oguzcantoptas@gmail.com>
2024-03-22 19:58:52 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+"use strict";
+console.log('index.ts is working!');
+var random20Number;
+(function (random20Number) {
+    /*
+      TypeScript'te class nedir?
+      nums = [5,7,9,1,4,8]
+      1 ile 10 arasında rastgele 20 adet sayı üret
+      1-)bu sayılardan verilen dizi içersinde olmayanların listesi
+      2-)bu sayılardan verilen dizi içerisinde olanların listesi
+      */
+    class NumberGenerator {
+        constructor() {
+            // private nums: number[] = [5, 7, 9, 1, 4, 8]
+            this.randomNumbers = [];
+        }
+        generateRandomNumbers() {
+            this.randomNumbers = [];
+            while (this.randomNumbers.length < 20) {
+                const number = Math.floor(Math.random() * 10) + 1;
+                this.randomNumbers.push(number);
+            }
+            return this.randomNumbers;
+        }
+        getMissingNumbers() {
+            const missingNumbers = [];
+            for (let i = 1; i <= 20; i++) {
+                if (!this.randomNumbers.includes(i)) {
+                    missingNumbers.push(i);
+                }
+            }
+            return missingNumbers;
+        }
+        getIncludedNumbers() {
+            const includedNumbers = [];
+            for (let i = 0; i < this.randomNumbers.length; i++) {
+                if (this.randomNumbers[i] >= 1 && this.randomNumbers[i] <= 10) {
+                    includedNumbers.push(this.randomNumbers[i]);
+                }
+            }
+            return includedNumbers;
+        }
+    }
+    const generator = new NumberGenerator();
+    const randomNumbers = generator.generateRandomNumbers();
+    console.log('randoms: ' + randomNumbers);
+    const missingNumbers = generator.getMissingNumbers();
+    console.log('missing: ' + missingNumbers);
+    const includedNumbers = generator.getIncludedNumbers();
+    console.log('included: ' + includedNumbers);
+})(random20Number || (random20Number = {}));
